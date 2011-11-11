b63a9d69b400cd578636a6a4148fb1e469c481c7
David McMahon <dmmcmah@c-174-56-68-164.hsd1.nm.comcast.net>
2011-11-10 19:04:02 -0700

    Initial Commit

---
+++ PlayAudio/AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.m
+//  PlayAudio
+//
+//  Created by David McMahon on 11/10/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
